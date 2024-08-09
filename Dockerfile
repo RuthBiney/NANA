272dd2989787dd286c462f69cf0bfe3a31f5dff2
RuthBiney <r.biney@alustudent.com>
2024-08-09 12:50:10 +0200

    updated these files

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 # Dockerfile
 FROM python:3.8-slim
 
-WORKDIR /app
+WORKDIR /mlapp
 
-COPY app /app
+COPY . /mlapp/
 
-RUN pip install --no-cache-dir -r /app/requirements.txt
+# Upgrade pip to the latest version
+RUN pip install --upgrade pip
+
+# Install dependencies with a longer timeout
+RUN pip install --default-timeout=100 --no-cache-dir -r /mlapp/app/requirements.txt
 
 CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import StandardScaler
 app = FastAPI()
 
 # Load the model
-model = joblib.load("/app/mlp_model.pkl")
+model = joblib.load("app/mlp_model.pkl")
 
 class Wine(BaseModel):
     fixed_acidity: float
@@ -36,8 +36,8 @@ def predict(wine: Wine):
 @app.post("/retrain/")
 def retrain():
     # Load and preprocess new data
-    red_wine = pd.read_csv('/app/winequality-red.csv', sep=';')
-    white_wine = pd.read_csv('/app/winequality-white.csv', sep=';')
+    red_wine = pd.read_csv('app/winequality-red.csv', sep=';')
+    white_wine = pd.read_csv('app/winequality-white.csv', sep=';')
 
     red_wine['wine_type'] = 'red'
     white_wine['wine_type'] = 'white'
@@ -52,6 +52,6 @@ def retrain():
     model.fit(X_scaled, y)
 
     # Save the updated model
-    joblib.dump(model, "/app/mlp_model.pkl")
+    joblib.dump(model, "app/mlp_model.pkl")
 
     return {"message": "Model retrained successfully"}
